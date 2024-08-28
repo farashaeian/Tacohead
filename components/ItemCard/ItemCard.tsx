b63dffa3bada252b8de4e350edc009f576faabeb
farashaeian <z.farashaeian@yahoo.com>
2024-08-28 13:51:53 +0330

    complete ItemCard component code

--- components/ItemCard/ItemCard.tsx
+++ components/ItemCard/ItemCard.tsx
@@ -11,15 +11,15 @@ interface Props {
 
 const ItemCard = ({ title, caption, price, imageUrl }: Props) => {
     return (
-        <div className="text-white p-1.5 md:p-3 flex flex-row md:flex-col gap-2.5 md:gap-3 bg-secondary-200 max-w-[394px] rounded-xl">
-            <div className="rounded-lg md:w-full md:max-h-52">
-                <Image src={imageUrl ?? PizzaImage} alt={title} className="w-full h-full object-cover rounded-lg"/>
+        <div className="text-white p-1.5 md:p-3 flex flex-row items-center justify-start md:flex-col md:justify-center md:items-center gap-2.5 md:gap-3 bg-secondary-200 max-w-[394px] rounded-xl">
+            <div className="rounded-lg w-[84px] h-[84px] md:w-full md:h-[200px]">
+                <Image src={imageUrl ?? PizzaMobileImage} alt={title} className="w-full h-full object-cover rounded-lg" />
             </div>
-            <div>
+            <div className="flex-1">
                 <h6 className="mb-1">{title}</h6>
-                <p className="mb-1 text-white-state-100 font-light text-[10px] md:text-xs leading-[14px]">{caption}</p>
+                <p className="mb-1 text-white-state-100 font-light text-[10px] md:text-xs leading-[14px] overflow-hidden text-ellipsis line-clamp-[1] md:overflow-visible md:line-clamp-none">{caption}</p>
                 <div className="flex justify-end items-end gap-0.5 md:gap-2.5">
-                    <span className="font-normal text-[21px] md:text-[23px] leading-[21px]">{price}</span> 
+                    <span className="font-normal text-[21px] md:text-[23px] leading-[21px]">{price}</span>
                     <div className="flex flex-col gap-0">
                         <span className="font-normal text-[8px] leading-[9.5px] md:text-[10px]">{thousandText}</span>
                         <span className="font-bold text-[8px] leading-[9.5px] md:text-[10px]">{tumanText}</span>
